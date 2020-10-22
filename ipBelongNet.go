a47f2556928c7e80eeb39ce889f64ad6e0b3de7a
zy <806897446@qq.com>
2020-10-22 18:35:32 +0800

    net ip

--- ipBelongNet.go
+++ ipBelongNet.go
@@ -9,8 +9,8 @@ func main() {
 	//network := &net.IPNet{IP: net.IPv4(10, 38, 0, 0), Mask: net.CIDRMask(16, 32)}
 	//ip := net.IPv4(10, 37, 163, 87)
 	//fmt.Println(network.Contains(ip))
-	i := "10.132.201.3"
-	n := "10.132.0.0/16"
+	i := ""
+	n := ""
 
 	ip := net.ParseIP(i)
 
