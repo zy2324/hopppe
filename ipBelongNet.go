c042a0e54c46d79bf01442237a2550a291d8648b
zy <806897446@qq.com>
2020-10-22 18:34:23 +0800

    net ip

--- ipBelongNet.go
+++ ipBelongNet.go
@@ -6,7 +6,14 @@ import (
 )
 
 func main() {
-	network := &net.IPNet{IP: net.IPv4(10, 38, 0, 0), Mask: net.CIDRMask(16, 32)}
-	ip := net.IPv4(10, 37, 163, 87)
-	fmt.Println(network.Contains(ip))
+	//network := &net.IPNet{IP: net.IPv4(10, 38, 0, 0), Mask: net.CIDRMask(16, 32)}
+	//ip := net.IPv4(10, 37, 163, 87)
+	//fmt.Println(network.Contains(ip))
+	i := "10.132.201.3"
+	n := "10.132.0.0/16"
+
+	ip := net.ParseIP(i)
+
+	_, network, _ := net.ParseCIDR(n)
+	fmt.Println(network.Contains(ip), network)
 }
